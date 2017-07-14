be6de2b0ff0fcb7604afb8c4342d876e710ee9b3
Peter-ODE <peter@ondatatransformation.net>
2017-07-14 17:24:33 +0100

    Removed UTF-8 fixup for discourse feed now we've moved to discourse 1.7

--- build.sh
+++ build.sh
@@ -10,7 +10,3 @@ if [[ -n "$WINDIR" ]]
 then
   find * -type f -exec unix2dos {} \; 
 fi
-
-# Discourse 1.6 doesn't like UTF-8 characters in the RSS feed (fixed in Discourse 1.7)
-iconv -f UTF-8 -t ISO-8859-15 --unicode-subst='?' discourse_feed.xml > discourse_feed.xml.new
-mv discourse_feed.xml.new discourse_feed.xml
