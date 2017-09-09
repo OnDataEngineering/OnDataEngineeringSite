70e49e4221f5fc824edc73117ea285e309bfbc6e
Peter-ODE <peter@ondatatransformation.net>
2017-09-09 22:12:38 +0100

    Rework of setup script

--- setup.sh
+++ setup.sh
@@ -1,110 +1,41 @@
-# Softlinks content site into this main site
-# See http://stackoverflow.com/questions/18641864/git-bash-shell-fails-to-create-symbolic-links
+#!/bin/bash -e
 
-# We still need this.
-windows() { [[ -n "$WINDIR" ]]; }
-
-# Cross-platform symlink function. Creates a symlink to a file or directory
-link() {
-  if windows; then
-    # Windows needs to be told if it's a directory or not. Infer that.
-    # Also: note that we convert `/` to `\`. In this case it's necessary.
-    if [[ -d "$1" ]]; then
-      cmd <<< "mklink /D \"$2\" \"${1//\//\\}\"" > /dev/null
-    else
-      cmd <<< "mklink \"$2\" \"${1//\//\\}\"" > /dev/null
-    fi
-  else
-    ln -s "$1" "$2"
-  fi
-}
-
-# Cross-platform symlink function. Deletes a symlink to a file or directory
-rmlink() {
-  if windows; then
-    # Windows needs to be told if it's a directory or not. Infer that.
-    # Also: note that we convert `/` to `\`. In this case it's necessary.
-    if [[ -d "$1" ]]; then
-      cmd <<< "rmdir \"${1//\//\\}\"" > /dev/null
-    else
-      cmd <<< "del \"${1//\//\\}\"" > /dev/null
-    fi
-  else
-    rm $1
-  fi
-}
-
-
-if [[ $# -ne 1 ]]
+if [[ $# -ne 2 ]]
 then
-  echo "Usage: `basename $0` <content_dir>"
+  echo "Usage: `basename $0` <content_dir> <site_dir>"
   exit -1
 fi
 
-contentdir=$1
-
-if [[ ! -d $contentdir ]]
+if [[ ! -d $1 ]]
 then
-  echo "$contentdir is not a directory"
+  echo "$1 is not a directory"
   exit -1
 fi
 
-for i in _drafts _posts _technologies _tech-categories _tech-vendors _templates _data _includes images redirects authors.yml
-do
-  echo
-  echo "Processing $i"
+if [[ ! -d $2 ]]
+then
+  echo "$2 is not a directory"
+  exit -1
+fi
 
+set -x
 
-  if [[ $i == "_templates" ]]
-  then
-    workdir="_includes"
-    src="../${contentdir}/$i"
-    trg="templates"
-  elif [[ $i == "_data" || $i == '_includes' ]]
-  then
-    workdir=$i
-    src="../${contentdir}/$i/shared"
-    trg="shared"
-  elif [[ $i == "authors.yml" ]]
-  then
-    workdir="_data"
-    src="shared/$i"
-    trg=$i
-  else
-    workdir="."
-    src="${contentdir}/$i"
-    trg=$i
-  fi
+# Link internal directories
+# We don't check these in as it causes Windows all sorts of issues
 
-  cd $workdir
+ln -snf _content/_drafts _drafts
+ln -snf _content/_posts _posts
+ln -snf _content/_technologies _technologies
+ln -snf _content/_tech-categories _tech-categories
+ln -snf _content/_tech-vendors _tech-vendors
+ln -snf ../_content/_templates _includes/templates
+ln -snf ../_content/_data/shared _data/shared
+ln -snf ../_content/_includes/shared _includes/shared
+ln -snf _content/images images
+ln -snf _content/redirects redirects
+ln -snf shared/authors.yml _data/authors.yml
 
-  if [[ -f $trg ]]
-  then
-    echo "Removing existing file $workdir/$trg"
-    rm $trg
-  if [[ $? -ne 0 ]]
-    then
-      exit -1
-    fi
-  fi  
+# Link content and site directories
 
-  if [[ -h $trg ]]
-  then 
-    echo "Removing existing symlink $workdir/$trg"
-    rmlink $trg
-    if [[ $? -ne 0 ]]
-    then
-      exit -1
-    fi
-  fi
-
-  echo "Creating link $src $workdir/$trg"
-  link $src $trg
-  if [[ $? -ne 0 ]]
-  then
-    exit -1
-  fi
-
-  cd - > /dev/null
-
-done
+ln -snf $1 _content
+ln -snf $2 _site
