bab6c0c25c52dd454ec06e1fca490011494157f3
Peter-ODE <peter@ondataengineering.net>
2017-11-03 17:05:53 +0000

    Update to test script to fail if errors are found

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 date
 
 # Run HTML proofer on generated _site directory, checking internal links only
-time bundle exec htmlproofer --assume-extension --internal-domains "ondataengineering.net" --disable-external --checks-to-ignore "ScriptCheck,ImageCheck" --allow-hash-href ./_site 2>&1 | tee output.log
+time bundle exec htmlproofer --assume-extension --internal-domains "ondataengineering.net" --disable-external --checks-to-ignore "ScriptCheck,ImageCheck" --allow-hash-href ./_site
 
 date
