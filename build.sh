73679ea5ef47c7ce6752d05486c99fccc666f9f3
Peter-ODE <peter@ondataengineering.net>
2018-10-12 17:38:18 +0100

    Update test script to error if Jekyll build emits silent errors

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 date
 
 # Build jekyll for production
-JEKYLL_ENV=production time bundle exec jekyll b $1
+JEKYLL_ENV=production time bundle exec jekyll b $1 | tee build.log
 
 date
--- test.sh
+++ test.sh
@@ -2,7 +2,15 @@
 
 date
 
+# Check build for errors
+grep "Error:" build.log || true
+
 # Run HTML proofer on generated _site directory, checking internal links only
 time bundle exec htmlproofer --assume-extension --internal-domains "ondataengineering.net" --disable-external --checks-to-ignore "ScriptCheck,ImageCheck" --allow-hash-href ./_site
+
+# Error if there were build errors
+if grep -q "Error:" build.log; then
+    exit 1
+fi
 
 date
