12dde99add6859b915a24d32d7aa2a459955419b
Peter @ OnDataEngineering.net <peter@ondataengineering.net>
2022-08-08 19:29:41 +0000

    Moved site, upgraded gems

--- setup.sh
+++ setup.sh
@@ -20,6 +20,11 @@ fi
 
 set -x
 
+# Link content and site directories
+
+ln -snf $1 _content
+ln -snf $2 _site
+
 # Link internal directories
 # We don't check these in as it causes Windows all sorts of issues
 
@@ -29,14 +34,8 @@ ln -snf _content/_technologies _technologies
 ln -snf _content/_tech-categories _tech-categories
 ln -snf _content/_tech-vendors _tech-vendors
 ln -snf ../_content/_templates _includes/templates
-ln -snf ../_content/_data/shared _data/shared
 ln -snf ../_content/_includes/shared _includes/shared
+ln -snf ../_content/_data/shared _data/shared
 ln -snf _content/images images
 ln -snf _content/indexes indexes
-ln -snf _content/tech-categories tech-categories
 ln -snf shared/authors.yml _data/authors.yml
-
-# Link content and site directories
-
-ln -snf $1 _content
-ln -snf $2 _site
